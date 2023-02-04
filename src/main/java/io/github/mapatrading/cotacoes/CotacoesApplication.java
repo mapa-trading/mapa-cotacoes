f26d008ea06d644f92a3aee2f30a85945470d57d
Alan Viana <alanviana.nascimento@gmail.com>
2023-02-04 16:51:08 -0300

    chore: Commit inicial

---
+++ src/main/java/io/github/mapatrading/cotacoes/CotacoesApplication.java
@@ -0,0 +1,13 @@
+package io.github.mapatrading.cotacoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CotacoesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CotacoesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/mapatrading/cotacoes/CotacoesApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.mapatrading.cotacoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CotacoesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
