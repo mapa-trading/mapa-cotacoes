fc8cd4bf4c70be8f9ea9ec53180afce6c6d66bc6
Alan Viana <alanviana.nascimento@gmail.com>
2023-02-05 18:18:19 -0300

    chore: ativos financeiros por tipo

--- src/main/java/io/github/mapatrading/cotacoes/controller/AtivoFinanceiroController.java
+++ src/main/java/io/github/mapatrading/cotacoes/controller/AtivoFinanceiroController.java
@@ -1,6 +1,7 @@
 package io.github.mapatrading.cotacoes.controller;
 
 import io.github.mapatrading.cotacoes.entity.AtivoFinanceiro;
+import io.github.mapatrading.cotacoes.enums.TipoAtivoFinanceiro;
 import io.github.mapatrading.cotacoes.repository.AtivoFinanceiroRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +9,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.springframework.http.HttpStatus.OK;
 
@@ -18,10 +22,27 @@ public class AtivoFinanceiroController {
     @Autowired
     private AtivoFinanceiroRepository ativoFinanceiroRepository;
 
+    @GetMapping
+    public ResponseEntity<List<AtivoFinanceiro>> getAll() {
+        List<AtivoFinanceiro> ativosFinanceiros = ativoFinanceiroRepository.findAll();
+
+        return new ResponseEntity<>(ativosFinanceiros, OK);
+    }
+
     @GetMapping("{sigla}")
     public ResponseEntity<AtivoFinanceiro> get(@PathVariable String sigla) {
         AtivoFinanceiro ativoFinanceiro = ativoFinanceiroRepository.findAtivoFinanceiroBySigla(sigla);
 
         return new ResponseEntity<>(ativoFinanceiro, OK);
+    }
+
+    @GetMapping("tipos/{tipo}")
+    public ResponseEntity<List<AtivoFinanceiro>> getAllByTipo(@PathVariable TipoAtivoFinanceiro tipo) {
+        List<AtivoFinanceiro> ativosFinanceiros = ativoFinanceiroRepository.findAll()
+                .stream()
+                .filter(ativoFinanceiro -> ativoFinanceiro.getTipoAtivo() == tipo)
+                .collect(Collectors.toList());
+
+        return new ResponseEntity<>(ativosFinanceiros, OK);
     }
 }
--- src/main/java/io/github/mapatrading/cotacoes/repository/AtivoFinanceiroRepository.java
+++ src/main/java/io/github/mapatrading/cotacoes/repository/AtivoFinanceiroRepository.java
@@ -1,9 +1,11 @@
 package io.github.mapatrading.cotacoes.repository;
 
 import io.github.mapatrading.cotacoes.entity.AtivoFinanceiro;
+import io.github.mapatrading.cotacoes.enums.TipoAtivoFinanceiro;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 
