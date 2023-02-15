da8437472618dbe85e3b3c613af0bdb23289fc31
Alan Viana <alanviana.nascimento@gmail.com>
2023-02-15 17:34:04 -0300

    chore: crud

--- src/main/java/io/github/mapatrading/cotacoes/controller/AcaoController.java
+++ src/main/java/io/github/mapatrading/cotacoes/controller/AcaoController.java
@@ -1,6 +1,7 @@
 package io.github.mapatrading.cotacoes.controller;
 
 import io.github.mapatrading.cotacoes.entity.Acao;
+import io.github.mapatrading.cotacoes.entity.AtivoFinanceiro;
 import io.github.mapatrading.cotacoes.repository.AcaoRepository;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
@@ -8,8 +9,10 @@ import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -44,12 +47,38 @@ public class AcaoController {
         return new ResponseEntity<>(acao, OK);
     }
 
+    @GetMapping("{id}")
+    public ResponseEntity<Acao> get(@PathVariable UUID id) {
+        Optional<Acao> acao = acaoRepository.findById(id);
+
+        if (acao.isPresent()){
+            return new ResponseEntity<>(acao.get(), OK);
+        } else return new ResponseEntity<>(NOT_FOUND);
+    }
+
     @DeleteMapping(value = "{id}")
     public ResponseEntity<Object> delete(@PathVariable(value = "id") UUID id) {
         Optional<Acao> acao = acaoRepository.findById(id);
         if (acao.isPresent()) {
             acaoRepository.delete(acao.get());
             return new ResponseEntity<>(OK);
+        } else return new ResponseEntity<>(NOT_FOUND);
+    }
+
+    @PutMapping(value = "{id}")
+    public ResponseEntity<Acao> edit(@PathVariable(value = "id") UUID id, @Valid @RequestBody Acao newAcao) {
+        Optional<Acao> oldAcao = acaoRepository.findById(id);
+
+        if(oldAcao.isPresent()){
+            Acao acao = oldAcao.get();
+            acao.setNome(newAcao.getNome());
+            acao.setDescricao (newAcao.getDescricao());
+            acao.setWebsite(newAcao.getWebsite());
+            acao.setMoeda(newAcao.getMoeda());
+            acao.setRazaoSocial(newAcao.getRazaoSocial());
+            acao.setSigla(newAcao.getWebsite());
+            acaoRepository.save(acao);
+            return new ResponseEntity<>(acao, OK);
         } else return new ResponseEntity<>(NOT_FOUND);
     }
 }
--- src/main/java/io/github/mapatrading/cotacoes/controller/CryptoController.java
+++ src/main/java/io/github/mapatrading/cotacoes/controller/CryptoController.java
@@ -5,8 +5,10 @@ import io.github.mapatrading.cotacoes.repository.CryptoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,12 +34,36 @@ public class CryptoController {
         return new ResponseEntity<>(crypto, OK);
     }
 
+    @GetMapping("{id}")
+    public ResponseEntity<Crypto> get(@PathVariable UUID id) {
+        Optional<Crypto> crypto = cryptoRepository.findById(id);
+
+        if (crypto.isPresent()){
+            return new ResponseEntity<>(crypto.get(), OK);
+        } else return new ResponseEntity<>(NOT_FOUND);
+    }
+
     @DeleteMapping(value = "{id}")
     public ResponseEntity<Object> delete(@PathVariable(value = "id") UUID id) {
         Optional<Crypto> crypto = cryptoRepository.findById(id);
         if (crypto.isPresent()) {
             cryptoRepository.delete(crypto.get());
             return new ResponseEntity<>(OK);
+        } else return new ResponseEntity<>(NOT_FOUND);
+    }
+
+    @PutMapping(value = "{id}")
+    public ResponseEntity<Crypto> edit(@PathVariable(value = "id") UUID id, @Valid @RequestBody Crypto newCrypto) {
+        Optional<Crypto> oldAcao = cryptoRepository.findById(id);
+
+        if(oldAcao.isPresent()){
+            Crypto crypto = oldAcao.get();
+            crypto.setNome(newCrypto.getNome());
+            crypto.setSigla (newCrypto.getSigla());
+            crypto.setImagemUrl(newCrypto.getImagemUrl());
+            crypto.setMoeda(newCrypto.getMoeda());
+            cryptoRepository.save(crypto);
+            return new ResponseEntity<>(crypto, OK);
         } else return new ResponseEntity<>(NOT_FOUND);
     }
 }
